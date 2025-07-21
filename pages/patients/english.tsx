c91c360752843f74ff16dfb36023fc24a0e6f4c5
synedu <34872906+synedu@users.noreply.github.com>
2025-07-22 01:40:04 +1000

    Remove login and redirect to patients

--- pages/patients/english.tsx
+++ pages/patients/english.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import Grid from '@mui/material/GridLegacy';
-import { useRouter } from 'next/router'
 import {
   Box,
   Container,
@@ -89,12 +88,6 @@ function EditableMDTMeeting() {
 }
 
 export default function EnglishPatientPage() {
-  const router = useRouter()
-  useEffect(() => {
-    if (typeof window !== 'undefined' && localStorage.getItem('loggedIn') !== 'true') {
-      router.replace('/login')
-    }
-  }, [router])
 
   // Static English patient data
   const patient = {
--- pages/patients/shepherd.tsx
+++ pages/patients/shepherd.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import Grid from '@mui/material/GridLegacy'
-import { useRouter } from 'next/router'
 import {
   Box,
   Container,
@@ -95,12 +94,6 @@ function EditableMDTMeeting() {
 }
 
 export default function ShepherdPatientPage() {
-  const router = useRouter()
-  useEffect(() => {
-    if (typeof window !== 'undefined' && localStorage.getItem('loggedIn') !== 'true') {
-      router.replace('/login')
-    }
-  }, [router])
 
   const patient = {
     name: 'Graham Shepherd',
