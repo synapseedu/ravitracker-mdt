67a67693537c6b81fcf97b41e4d9d7027941d921
synedu <34872906+synedu@users.noreply.github.com>
2025-08-11 15:36:01 +1000

    Update patient list index

--- data/patients/index.ts
+++ data/patients/index.ts
@@ -1,22 +1,12 @@
 import type { Patient } from './types';
 export type { Patient } from './types';
+
 import newbould from './newbould';
 import stevenson from './stevenson';
 import bogle from './bogle';
 import smithm from './smithm';
-import mcguire from './mcguire';
-import nas from './nas';
-import newlands from './newlands';
 import shepherd from './shepherd';
-import russ from './russ';
-import vandevelde from './vandevelde';
 import gaffney from './gaffney';
-import mooney from './mooney';
-import sorentino from './sorentino';
-import austin from './austin';
-import moelle from './moelle';
-import low from './low';
-import dunbar from './dunbar';
 import washington from './washington';
 
 export const allPatients: Patient[] = [
@@ -24,19 +14,8 @@ export const allPatients: Patient[] = [
   stevenson,
   bogle,
   smithm,
-  mcguire,
-  nas,
-  newlands,
   shepherd,
-  russ,
-  vandevelde,
   gaffney,
-  mooney,
-  sorentino,
-  austin,
-  moelle,
-  low,
-  dunbar,
   washington,
 ];
 
